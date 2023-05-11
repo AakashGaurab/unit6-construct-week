26415a5b60acbaab82a8545ff5cf3e5099b5b6bf
Aakash <aakashgaurav456@gmail.com>
2023-05-11 21:40:25 +0530

    Database url added in .env

--- config/db.js
+++ config/db.js
@@ -1,16 +1,16 @@
-// const sequelize = require("sequelize");
-// const seq = new sequelize('hospital_appointment_booking_system', 'root', 'Mysql@0410', {
-//     host: 'localhost',
-//     dialect: 'mysql'
-// });
+const sequelize = require("sequelize");
+const seq = new sequelize('hospital_appointment_booking_system', 'root', 'Mysql@0410', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
 
 
-// module.exports = { seq };
-require('dotenv').config()
-const mysql = require('mysql2')
-const connection = mysql.createConnection(process.env.DATABASE_URL)
+module.exports = { seq };
+// require('dotenv').config()
+// const mysql = require('mysql2')
+// const connection = mysql.createConnection(process.env.DATABASE_URL)
 
-connection.connect();
+// connection.connect();
 
 
-module.exports = { connection };
+// module.exports = { connection };
